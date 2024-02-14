a73082f8e867f150cd091b4cab4146b586af575e
Samuel Noah Rosean <44511735+samrosean@users.noreply.github.com>
2024-02-14 11:52:58 -0500

    update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = 'Compare ATAC-seq datasets against loci datasets to see if th
   
 setup(
         name ='gaiaAssociation',
-        version ='1.1.2',
+        version ='1.2.0',
         author ='Samuel Rosean',
         author_email ='samuel.rosean@einsteinmed.edu',
         url ='https://github.com/samrosean/gaiaAssociation',
